1f0007a038c9e320b2b7a19029595440f2c53ecf
YouSlovenia <loverussiamapping656@gmail.com>
2024-09-22 09:04:07 -0300

    Create CopnerightZoght.js

---
+++ CopnerightZoght.js
@@ -0,0 +1,26 @@
+import {CustomCost, ExponentialCost} from "./api/Costs";
+import { Localization } from "./api/Localization";
+import {BigNumber, parseBigNumber} from "./api/BigNumber";
+import {QuaternaryEntry, theory} from "./api/Theory";
+import {Utils} from "./api/Utils";
+import {ui} from "./api/ui/UI";
+import {Thickness} from "./api/ui/properties/Thickness";
+import {TextAlignment} from "./api/ui/properties/TextAlignment";
+import {FontAttributes} from "./api/ui/properties/FontAttributes";
+import {TouchType} from "./api/ui/properties/TouchType";
+import { Vector3 } from "../../../Projects/theory-sdk/api/Vector3";
+import {Color} from "./api/ui/properties/Color";
+import {CornerRadius} from "./api/ui/properties/CornerRadius";
+import {game} from "./api/Game";
+
+requiresGameVersion("1.4.33");
+
+var id = "numberinc5";
+var name = "Number Incremental 5";
+var description = "Under constuction"
+var authors = "Me";
+var version = 1;
+
+var init = () => {
+    currency = theory.createCurrency();
+}
