c291dae23046297807a6d45a814a0f3e7098ea6a
YouSlovenia <loverussiamapping656@gmail.com>
2024-10-27 12:37:13 -0300

    Update NorthFunction.js

--- NorthFunction.js
+++ NorthFunction.js
@@ -35,7 +35,7 @@ var init = () => {
     {
         let getDesc = (level) => "c_2=2^{" + level + "}";
         let getInfo = (level) => "c_2=" + getC2(level).toString(0);
-        c2 = theory.createUpgrade(1, currency, new ExponentialCost(20, Math.log2(10)));
+        c2 = theory.createUpgrade(1, currency_N, new ExponentialCost(20, Math.log2(10)));
         c2.getDescription = (_) => Utils.getMath(getDesc(c2.level));
         c2.getInfo = (amount) => Utils.getMathTo(getInfo(c2.level), getInfo(c2.level + amount));
     }
